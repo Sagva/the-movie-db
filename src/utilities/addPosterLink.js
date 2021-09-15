397036eb833d38aa3fdae44bfaf7e32e66cbd240
Elena <sagva2014@gmail.com>
2021-09-15 21:31:31 +0200

    Fixed bag with poster link

--- src/utilities/addPosterLink.js
+++ src/utilities/addPosterLink.js
@@ -1,8 +1,8 @@
-
-
 export const addPosterLink = (arr) => {
     const moviesWithPosters = arr.map((movie) => {
-        movie.posterLink = `https://image.tmdb.org/t/p/w154/${movie.poster_path}`
+        if(movie.poster_path) {
+            movie.posterLink = `https://image.tmdb.org/t/p/w154/${movie.poster_path}`
+        }
         return movie
     })
     return moviesWithPosters
