964664ae21ae8ef029282fd2574aae7ded7aba97
Elena <sagva2014@gmail.com>
2021-09-16 19:25:45 +0200

    Added comments to MoviesByGenrePage

--- src/pages/MoviesByGenrePage.jsx
+++ src/pages/MoviesByGenrePage.jsx
@@ -9,51 +9,52 @@ import { useQueryParam, NumberParam } from 'use-query-params';
     
 
 const MoviesByGenrePage = () => {
-    let location = useLocation(); 
-    const [pageParam, setPageParam] = useQueryParam('page', NumberParam);
+
+    //for working back-button in the browser when pagination buttons are used
+    let location = useLocation() // for looking after changing of url
+    const [pageParam, setPageParam] = useQueryParam('page', NumberParam);//ads '?page=' to url, undefined in the beginig
     const [currentPage, setCurrentPage] = useState(1)
     
+    useEffect(() => {//first rendering of page  
+        if(!pageParam) { //if pageParam is undefined then set it to 1
+            setPageParam(1)
+        } else {//if user types url (and a page number) by hand, then we get 'page' from url and set it as a pageParam 
+            setPageParam(parseInt(location.search.match(/([\d]+)/g)))
+        }
+    }, []);
 
-    useEffect(() => {
-        if(pageParam && currentPage !== pageParam) {
+    useEffect(() => {//every time when the pageParam is changing (when user types url (and a page number) by hand), check if pageParam is the same as currentPage. If not make current page equal to pageParam 
+        if(pageParam && currentPage !== pageParam) { 
             setCurrentPage(pageParam)
         }
     }, [pageParam]);
 
-    useEffect(() => {
+    useEffect(() => {//every time when the currentPage is changing (when butten "Next" in PaginationComponent is clicked), check if currentPage is the same as pageParam. If not make pageParam equal to currentPage 
         if(currentPage && currentPage !== pageParam) {
             setPageParam(currentPage)
         } 
     }, [currentPage]);
 
       
-    useEffect(() => {
-        if(!pageParam) {
-            setPageParam(1)
-        } else {
-            setPageParam(parseInt(location.search.match(/([\d]+)/g)))
-        }
-         
-    }, []);
-
-    const { id } = useParams() //genre's id
-    const { data: genres} = useQuery([`genresList`], getAllGenres)
+    //for making query to API and rendering the result
+    const { id } = useParams() //get genre's id
+    const { data: genres} = useQuery([`genresList`], getAllGenres)//query to get all genres names from API
     const [genreName, setGenreName] = useState(null)
     
-    useEffect(() => {
+    useEffect(() => {//as soon as all genres names and chosen genre Id are gotten, find name of chosen genre and set it to the state (so we can render on the page genre's name)
         if(genres) {
-            setGenreName(findGengeName(genres, id))
+            setGenreName(findGengeName(genres, id)) //in the help function uses filter method
         }
     }, [genres, id]);
-
     
+    //make query to get movies by genre's id from API, with current page
     const { data, isError } = useQuery([`moviesByGenre-${id}`, { id, currentPage }], () => getMoviesByGenre(id, currentPage))
     
 
     const paginationValues = {
-        currentPage: currentPage,
-        setCurrentPage: setCurrentPage,
-        totalPages: data?.total_pages
+        currentPage: currentPage,//for rendering number of the page the user is it on
+        setCurrentPage: setCurrentPage, //for changing number of page when the user preses button 'next' or 'previous' in the pagination
+        totalPages: data?.total_pages //for blocking button 'next' when the user is on the last page
     }
     
     const renderMovieList = () => {
